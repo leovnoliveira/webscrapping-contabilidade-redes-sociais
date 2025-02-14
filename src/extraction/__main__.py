4abc24de0f52f0cf0385a740bf50ec3098883a97
leovnoliveira <consultoria1.ceo@gmail.com>
2025-02-14 13:20:17 -0400

    Melhorias na arquitetura do código

--- src/extraction/__main__.py
+++ src/extraction/__main__.py
@@ -1,26 +1,20 @@
 from src.extraction.x_extraction import extrair_dados_x
 from src.extraction.youtube_extraction import extrair_dados_youtube
-from src.utils.__init__ import *
-from src.utils.config import carregar_variaveis_sensiveis
-
+from src.utils import *
 
 if __name__ == "__main__":
 
-    load_dotenv()
-
     config = carregar_variaveis_sensiveis()
+    print("Rodando extração de dados...")
 
-    # --- EXTRAÇÃO ---
-    # Dados do YouTube
-    VIDEO_ID = "Js8yNAOTws8"  # Substitua pelo ID real do vídeo
+    # Exemplo de execução
+    VIDEO_ID = "Js8yNAOTws8"
     video_data, comentarios = extrair_dados_youtube(config["YOUTUBE_API_KEY"], VIDEO_ID)
-    
-    # Dados do Twitter
+
     tweets = extrair_dados_x(
         config["BEARER_TOKEN"],
-        config["CONSULTA_TWITTER"],
+        config["CONSULTA_X"],
         max_results= 10
     )
 
-    for tweet in tweets:
-        print(tweets.text)
+    print(f"Total de tweets extraídos: {len(tweets)}")
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 if __name__ == "__main__":
 
     load_dotenv()
+    api_key = os.getenv("YOUTUBE_API_KEY")
+    if not api_key:
+        raise ValueError("YOUTUBE_API_KEY não está definida! Verifique seu arquivo .env.")
+
+    print("YOUTUBE_API_KEY =", api_key)
 
     config = carregar_variaveis_sensiveis()
     BEARER_TOKEN = config["BEARER_TOKEN"]
-    CONSULTA_TWITTER = config["CONSULTA_TWITTER"]
+    CONSULTA_X = config["CONSULTA_X"]
